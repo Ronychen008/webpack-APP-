c158fe353f26f1b4955c481b226b4f4f20d0fbe6
chenchong <1058940453@qq.com>
2019-07-03 07:44:51 +0800

    修改了main.js文件

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'mint-ui/lib/style.css'
 import "./lib/css/mui.css"
 Vue.component(Header.name,Header);
 import app from "./app.vue"
+//这里千万不能忘记放到window.onload中
 window.onload=function () {
     var vm =new Vue({
         el:'#demo',
